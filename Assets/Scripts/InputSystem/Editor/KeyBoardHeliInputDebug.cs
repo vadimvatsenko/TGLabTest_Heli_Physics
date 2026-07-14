5acd736e3fde2642d61fcddd8804408ccb8854ac
WadeWilson228 <69381493+vadimvatsenko@users.noreply.github.com>
2026-07-14 19:45:42 +0300

    add editor helper for check input

---
+++ Assets/Scripts/InputSystem/Editor/KeyBoardHeliInputDebug.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+
+namespace InputSystem.Editor
+{
+    [CustomEditor(typeof(BaseHeliInput))]
+    public class KeyBoardHeliInputDebug : UnityEditor.Editor
+    {
+        private BaseHeliInput _input;
+
+        private void OnEnable() => _input = target as BaseHeliInput;
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            DrawDebugUI();
+            Repaint();
+        }
+
+        private void DrawDebugUI()
+        {
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            EditorGUILayout.LabelField("Input System", EditorStyles.boldLabel);
+            EditorGUILayout.Space();
+            EditorGUI.indentLevel++; // падінг
+            EditorGUILayout.LabelField($"ThrottleInput: {_input.ThrottleInput}", EditorStyles.boldLabel);
+            EditorGUILayout.LabelField($"CollectiveInput: {_input.CollectiveInput}", EditorStyles.boldLabel);
+            EditorGUILayout.LabelField($"CyclicInput: {_input.CyclicInput}", EditorStyles.boldLabel);
+            EditorGUILayout.LabelField($"PedalInput: {_input.PedalInput}", EditorStyles.boldLabel);
+            EditorGUILayout.Space();
+            EditorGUI.indentLevel--;
+            EditorGUILayout.EndVertical();
+        }
+    }
+}
