c57bb6e1474fe2c9139eab411c46db663fbf9b90
WadeWilson228 <69381493+vadimvatsenko@users.noreply.github.com>
2026-07-18 16:28:36 +0300

    add exit and reload btns

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -2,6 +2,8 @@
 using Rotors;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace UI
 {
@@ -11,13 +13,17 @@ namespace UI
         [SerializeField] private MainRotor mainRotor;
         [SerializeField] private TextMeshProUGUI textHp;
         [SerializeField] private TextMeshProUGUI textRpm;
-        [SerializeField] private TextMeshProUGUI textBladesAngle; 
+        [SerializeField] private TextMeshProUGUI textBladesAngle;
+        [SerializeField] private Button exitButton;
+        [SerializeField] private Button restartButton;
 
         private void OnEnable()
         {
             mainHeliEngine.OnChangeCurrentHp += OnTextHpChanged;
             mainHeliEngine.OnChangeCurrentRpm += OnTextRpmChanged;
             mainRotor.OnRotate += OnTextBladesAngleChanged;
+            exitButton.onClick.AddListener(OnExitButton);
+            restartButton.onClick.AddListener(OnRestartButton);
         }
 
         private void OnDisable()
@@ -25,6 +31,8 @@ namespace UI
             mainHeliEngine.OnChangeCurrentHp -= OnTextHpChanged;
             mainHeliEngine.OnChangeCurrentRpm -= OnTextRpmChanged;
             mainRotor.OnRotate -= OnTextBladesAngleChanged;
+            exitButton.onClick.RemoveListener(OnExitButton);
+            restartButton.onClick.RemoveListener(OnRestartButton);
         }
 
         private void OnTextHpChanged(float text) => textHp.text = $"HP : {text.ToString()}";
@@ -32,5 +40,8 @@ namespace UI
 
         private void OnTextBladesAngleChanged(float text) =>
             textBladesAngle.text = $"Blades Angle: {text.ToString()}";
+        
+        private void OnExitButton() => Application.Quit();
+        private void OnRestartButton() => SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
