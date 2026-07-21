153aa5fc905a3ac883b9fa2f696004255d23bd9b
WadeWilson228 <69381493+vadimvatsenko@users.noreply.github.com>
2026-07-21 18:46:40 +0300

    add some objects

--- Assets/Scripts/Rotors/AdvanceMainRotor.cs
+++ Assets/Scripts/Rotors/AdvanceMainRotor.cs
@@ -4,25 +4,29 @@ using UnityEngine;
 
 namespace Rotors
 {
-    public class MainRotor : MonoBehaviour, IRotor
+    public class AdvanceMainRotor : MonoBehaviour, IRotor
     {
-        [Header("Main Rotor Properties")] 
         [SerializeField] protected Transform lFirstRotor;
+        [SerializeField] protected Transform lSecondRotor;
         [SerializeField] protected Transform rFirstRotor;
-        // максимальний кут леза
+        [SerializeField] protected Transform rSecondRotor;
+
         [SerializeField] protected float maxPitch = 35f;
 
         public Action<float> OnRotate;
+
         public void UpdateRotor(float dps, BaseHeliInput input)
         {
             float pitch = input.CollectiveInput * maxPitch;
             OnRotate?.Invoke(pitch);
             // оберт 
             transform.Rotate(Vector3.up, dps);
-            if (lFirstRotor && rFirstRotor)
+            if (lFirstRotor && rFirstRotor && lSecondRotor && rSecondRotor)
             {
                 lFirstRotor.localRotation = Quaternion.Euler(0f, 0f, pitch);
+                lSecondRotor.localRotation = Quaternion.Euler(0f, 0f, pitch);
                 rFirstRotor.localRotation = Quaternion.Euler(0f, 0f, -pitch);
+                rSecondRotor.localRotation = Quaternion.Euler(0f, 0f, -pitch);
             }
         }
     }
